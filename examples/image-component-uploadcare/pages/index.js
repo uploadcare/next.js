8cf5262f4c28f1da4316ac7190704e77957738ad
nd0ut <nd0ut.me@gmail.com>
2020-10-30 15:21:55 +0300

    Add example for uploadcare loader

---
+++ examples/image-component-uploadcare/pages/index.js
@@ -0,0 +1,36 @@
+import styles from '../styles.module.css'
+import Image from 'next/image'
+
+const Code = (p) => <code className={styles.inlineCode} {...p} />
+
+const Index = () => (
+  <div className={styles.container}>
+    <div className={styles.card}>
+      <h1>Uploadcare loader for Image Component</h1>
+      <p>
+        The following is an example of a reference to an image from the{' '}
+        Uploadcare CDN at <Code>ucarecdn.com</Code>
+      </p>
+      <Image alt="Vercel logo" src="https://ucarecdn.com/a6f8abc8-f92e-460a-b7a1-c5cd70a18cdb/vercel.png" width={1000} height={1000} />
+      <hr className={styles.hr} />
+      <p>
+        The following is an example of a reference to an external image at{' '}
+        <Code>assets.vercel.com</Code>.
+      </p>
+      <Image
+        alt="Next.js logo"
+        src="https://assets.vercel.com/image/upload/v1538361091/repositories/next-js/next-js.png"
+        width={1200}
+        height={400}
+      />
+      <hr className={styles.hr} />
+      Checkout the documentation for{' '}
+      <a href="https://nextjs.org/docs/basic-features/image-optimization#uploadcare-loader">
+        Image Optimization
+      </a>{' '}
+      to learn more.
+    </div>
+  </div>
+)
+
+export default Index
